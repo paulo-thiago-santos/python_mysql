73c18f5475c040f9fd40157e394a301838deae87
paulo-thiago-santos <paulo.thiago.santos@edu.sc.senai.br>
2026-09-22 18:20:30 -0300

    Update main.py

--- main.py
+++ main.py
@@ -7,6 +7,24 @@ conexao = mysql.connector.connect(
     database="escola"
 )
 
-cursor = conexao.cursor()
+terminal = conexao.cursor()
 
-cursor.execute("SELECT * FROM alunos")
+ALUNO = "<NOME_DO_ALUNO>"
+
+# CREATE DATABASE
+
+str_to_execute = f"CREATE DATABASE {ALUNO}_db;"
+terminal.execute(str_to_execute)
+
+str_to_execute = f"USE {ALUNO}_db"
+terminal.execute(str_to_execute)
+
+# CREATE TABLE
+str_to_execute = """
+    CREATE TABLE alunos (
+        id INT PRIMARY KEY AUTO_INCREMENT,
+        nome VARCHAR(100) NOT NULL,
+        email VARCHAR(150)
+    )
+"""
+terminal.execute(str_to_execute)
